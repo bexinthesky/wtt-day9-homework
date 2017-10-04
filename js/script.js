89f3338bcf2f9713813ee7476f077449d785ce95
Rebecca Horwitz <rebeccahorwitz@Rebeccas-MacBook-Pro.local>
2017-10-04 14:29:54 -0700

    started boxes

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Create some gray (#ccc) boxes in html (at least 6 boxes)
+// (100px wide and 100px high should be good)
+// target the boxes in the HTML
+// add event listener / function so that when you click on a box, the background color changes
+// then, make it so that if a user clicks on a box a second time, change the border radius of the box
+
+// BONUS: Make it so only one box can be your new color at a time
